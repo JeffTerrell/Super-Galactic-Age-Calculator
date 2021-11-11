e4122715847ca1f7c298a31239804e90d2e37c60
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-11 12:13:36 -0500

    add GalacticAge test2 to agecalc.test.js

--- __tests__/agecalc.test.js
+++ __tests__/agecalc.test.js
@@ -6,6 +6,12 @@ describe('GalacticAge', () => {
 
   test('should correctly create an object from user inputted planet', () => {
     const planet = new GalacticAge("Mercury");
-    expect(planet.userPlanet).toEqual('Mercury');  
+    expect(planet.userPlanet).toEqual("Mercury");  
   });
+
+  test('should correctly create an object from user inputted planet and age', () => {
+    const planet = new GalacticAge("Mercury", 20)
+    expect(planet.userPlanet).toEqual("Mercury");
+    expect(planet.userAge).toEqual(20);
+  })
 });    

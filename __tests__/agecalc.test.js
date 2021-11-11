bafa9ec246224c5b865a96e899e7d912b0fc6000
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-11 12:45:49 -0500

    delete unneeded test1 and new test1 in agecalc.test.js

--- __tests__/agecalc.test.js
+++ __tests__/agecalc.test.js
@@ -2,16 +2,8 @@ import GalacticAge from './../src/js/agecalc.js';
 
 describe('GalacticAge', () => {
 
-
-
-  test('should correctly create an object from user inputted planet', () => {
-    const planet = new GalacticAge("Mercury");
-    expect(planet.userPlanet).toEqual("Mercury");  
-  });
-
-  test('should correctly create an object from user inputted planet and age', () => {
-    const planet = new GalacticAge("Mercury", 20)
-    expect(planet.userPlanet).toEqual("Mercury");
+  test('should correctly create an object from user age', () => {
+    const planet = new GalacticAge(20)
     expect(planet.userAge).toEqual(20);
   })
 });    

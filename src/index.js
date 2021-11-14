95ff0360d86042aa8d48787047feb2458e7eb672
Jeff Terrell <jeffaterrell@gmail.com>
2021-11-13 22:53:07 -0500

    update UI logic to show and hide error messages correctly

--- src/index.js
+++ src/index.js
@@ -11,68 +11,102 @@ $(document).ready(function() {
   $("#agelifeform").submit(function(event) {
     event.preventDefault();
     const age = parseInt($("#age").val());
-    console.log(age);
     const lifeExpect = parseFloat($("#life-expect").val());
-    console.log(lifeExpect);
     const ageLife = new GalacticAge(age, lifeExpect);
-    console.log(ageLife.mercury);
-    const ageLifeMercury = ageLife.mercuryLifeExpectancy();
-    const ageLifeVenus = ageLife.venusLifeExpectancy();
-    const ageLifeMars = ageLife.marsLifeExpectancy();
-    const ageLifeJupiter = ageLife.jupiterLifeExpectancy();
+    const lifeMercury = ageLife.mercuryLifeExpectancy();
+    const lifeVenus = ageLife.venusLifeExpectancy();
+    const lifeMars = ageLife.marsLifeExpectancy();
+    const lifeJupiter = ageLife.jupiterLifeExpectancy();
 
-  
-  //   *** WORK IN PROGRESS ***  
-  //   function displayMessage () {
-  //     $("#mercuryage").html(this.mercury) // use as span on index.html
-  //     $("#mercurylife").html(ageLifeMercury) // use as span on index.html
-  //     if (this.mercury < this.userLifeExpectancy/.24) {
-  //      $("#mercury1").show();
-  //     }
-  //      else {
-  //      $("#mercury2").show();
-  //   }
-  // }
-
-    function displayMercuryMessage () {
-      if (ageLife.mercury < ageLife.userLifeExpectancy/.24) {
-        $("#mercury1").show();
-        $("#mercury2").hide();
-        return $("#mercury1-1").text("Your age on Mercury is: " + ageLife.mercury + ". You are expected to live another " + ageLifeMercury + " years on Mercury.");
-      }  
-      else {
-        $("#mercury2").show();
-        $("#mercury1").hide();
-        return $("#mercury2-2").text("Your age on Mercury is: " + ageLife.mercury + ". Congrats! You have lived " + ageLifeMercury + " years past average life expectancy on Mercury.");
+    function errorMessage() {
+      if (isNaN(age) && isNaN(lifeExpect)) {
+        $("#planets").hide();
+        $("#errorage").hide();
+        $("#errorlife").hide();
+        return $("#erroragelife").show();
       }
+        else if (isNaN(age)) {
+          $("#planets").hide();
+          $("#erroragelife").hide();
+          $("#errorlife").hide();
+          return $("#errorage").show();
+      }
+        else if (isNaN(lifeExpect)) {
+          $("#planets").hide();
+          $("#erroragelife").hide();
+          $("#errorage").hide();
+          return $("#errorlife").show();
+        }
+    }
+    function displayMercuryMessage() {
+      if (errorMessage());
+        else if (ageLife.mercury < ageLife.userLifeExpectancy/.24) {
+          $("#erroragelife").hide();
+          $("#errorage").hide();
+          $("#errorlife").hide();
+          $("#planets").show();
+          $("#mercury1").show();
+          $("#mercury2").hide();
+          $("#mercury1-age").text(ageLife.mercury);
+          $("#mercury1-life").text(lifeMercury);
+        }  
+        else {
+          $("#erroragelife").hide();
+          $("#errorage").hide();
+          $("#errorlife").hide();
+          $("#planets").show();
+          $("#mercury2").show();
+          $("#mercury1").hide();
+          $("#mercury2-age").text(ageLife.mercury);
+          $("#mercury2-life").text(lifeMercury);
+        }
+    }
+    function displayVenusMessage() {
+      if (errorMessage());
+        else if (ageLife.venus < ageLife.userLifeExpectancy/.62) {
+          $("#venus1").show();
+          $("#venus2").hide();
+          $("#venus1-age").text(ageLife.venus);
+          $("#venus1-life").text(lifeVenus);
+        }
+        else {
+          $("#venus2").show();
+          $("#venus1").hide();
+          $("#venus2-age").text(ageLife.venus);
+          $("#venus2-life").text(lifeVenus);
+        }
+    }
+    function displayMarsMessage() {
+      if (errorMessage());
+        else if (ageLife.mars < ageLife.userLifeExpectancy/1.88) {
+          $("#mars1").show();
+          $("#mars2").hide();
+          $("#mars1-age").text(ageLife.mars);
+          $("#mars1-life").text(lifeMars);
+        }
+        else {
+          $("#mars2").show();
+          $("#mars1").hide();
+          $("#mars2-age").text(ageLife.mars);
+          $("#mars2-life").text(lifeMars);
+        }
+    }
+    function displayJupiterMessage() {
+      if (errorMessage());
+        else if (ageLife.jupiter < ageLife.userLifeExpectancy/11.86) {
+          $("#jupiter1").show();
+          $("#jupiter2").hide();
+          $("#jupiter1-age").text(ageLife.jupiter);
+          $("#jupiter1-life").text(lifeJupiter);
+        }
+        else {
+          $("#jupiter2").show();
+          $("#jupiter1").hide();
+          $("#jupiter2-age").text(ageLife.jupiter);
+          $("#jupiter2-life").text(lifeJupiter);
+        }
     }
 
-    function displayVenusMessage () {
-      if (ageLife.venus < ageLife.userLifeExpectancy/.62) {
-        return $("#venus1").text("<p>Your age on Venus is: " + ageLife.venus + ". You are expected to live another " + ageLifeVenus + " years on Venus. <p>");
-      }  
-      else {
-        return $("#venus2").text("<p>Your age on Venus is: " + ageLife.venus + ". Congrats! You have lived " + ageLifeVenus + " years past average life expectancy on Venus. </p>");
-      }
-    }
-
-    function displayMarsMessage () {
-      if (ageLife.mars < ageLife.userLifeExpectancy/1.88) {
-        return $("#mars1").text("<p>Your age on Mars is: " + ageLife.mars + ". You are expected to live another " + ageLifeMars + " years on Mars. <p>");
-      }  
-      else {
-        return $("#mars2").text("<p>Your age on Mars is: " + ageLife.mars + ". Congrats! You have lived " + ageLifeMars + " years past average life expectancy on Mars. </p>");
-      }
-    }
-
-    function displayJupiterMessage () {
-      if (ageLife.jupiter < ageLife.userLifeExpectancy/11.86) {
-        return $("#jupiter1").text("<p>Your age on Jupiter is: " + ageLife.jupiter + ". You are expected to live another " + ageLifeJupiter + " years on Jupiter <p>");
-      }  
-      else {
-        return $("#jupiter2").text("<p>Your age on Jupiter is: " + ageLife.jupiter + ". Congrats! You have lived " + ageLifeJupiter + " years past average life expectancy on Jupiter </p>");
-      }
-    }
     displayMercuryMessage();
     displayVenusMessage();
     displayMarsMessage();
